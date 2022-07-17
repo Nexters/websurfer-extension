6b43ac9ca0204c1fd59f4e9ae4cf50772317b868
DABIN JEONG <41@b41.kr>
2022-07-17 09:02:00 +0900

    add reducer counter

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from '../../redux/store';
 
 import Newtab from './Newtab';
 
-render(<Newtab />, window.document.querySelector('#app-container'));
+render(
+  <Provider store={store}>
+    <Newtab />
+  </Provider>,
+  window.document.querySelector('#app-container')
+);
 
 if (module.hot) module.hot.accept();
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from '../../redux/store';
 
 import Popup from './Popup';
 
-render(<Popup />, window.document.querySelector('#app-container'));
+render(
+  <Provider store={store}>
+    <Popup />
+  </Provider>,
+  window.document.querySelector('#app-container')
+);
 
 if (module.hot) module.hot.accept();
