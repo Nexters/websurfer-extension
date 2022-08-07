935b28be7df4aed45eed4728da33c1c27a14a6de
DABIN JEONG <41@b41.kr>
2022-08-07 22:06:44 +0900

    style modify bar chart style
    - x축 하단 선 변경
    - y축 단위 표시 삭제
    - 카테고리 symbol 추가 및 색상 적용

--- src/components/Main/MainContent/SurffingTime.tsx
+++ src/components/Main/MainContent/SurffingTime.tsx
@@ -12,24 +12,43 @@ const SurffingTime = (props: Props) => {
   const theme = useTheme();
   const option = {
     grid: {
-      left: '10%',
-      right: '10%',
-      top: 10,
-      bottom: 20,
+      left: '12px',
+      right: '12px',
+      top: 40,
+      bottom: 0,
+      width: '222px',
+      height: '110px',
     },
     xAxis: {
       type: 'category',
       data: ['아침', '낮', '저녁', '밤'],
+      axisLabel: {
+        color: theme.color.black,
+      },
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: theme.color.secondaryB,
+        },
+      },
+      axisTick: false,
     },
     yAxis: {
       type: 'value',
+      splitLine: {
+        show: false,
+      },
+      axisLabel: false,
     },
     series: [
       {
         data: [
           100,
           100,
-          { value: 100, itemStyle: { color: theme.color.primary } },
+          {
+            value: 120,
+            itemStyle: { color: theme.color.primary },
+          },
           100,
         ],
         itemStyle: {
@@ -38,16 +57,27 @@ const SurffingTime = (props: Props) => {
         },
         type: 'bar',
         silent: true,
+        barWidth: '30px',
+        barGap: '34px',
+        markPoint: {
+          data: [
+            {
+              type: 'max',
+              itemStyle: { color: theme.color.secondaryY },
+              label: { show: false },
+            },
+          ],
+          symbol: 'circle',
+          symbolSize: 30,
+          symbolOffset: [0, -20],
+        },
       },
     ],
   };
   return (
-    <Card.Wrapper height="308px" width="400px" borderRadius="8px">
+    <Card.Wrapper height="250px" width="290px" borderRadius="8px">
       <Card.Title>자주 사용한 시간대</Card.Title>
-      <ReactECharts
-        option={option}
-        style={{ height: '200px', width: 'auto' }}
-      />
+      <ReactECharts option={option} style={{ height: '168px' }} />
     </Card.Wrapper>
   );
 };
