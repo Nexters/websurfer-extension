18a0860181b203fdbefda5d7db74f5051294c012
DABIN JEONG <41@b41.kr>
2022-08-08 01:33:56 +0900

    add connect query to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
 import counterReducer from './counterSlice';
+import { webSerferApi } from './services/webSerfer';
 
 export const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: {
+    [webSerferApi.reducerPath]: webSerferApi.reducer,
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(webSerferApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
