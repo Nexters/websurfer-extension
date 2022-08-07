16b0dce16e3889e7f5a5e5e94c71ea727533e95e
DABIN JEONG <41@b41.kr>
2022-08-08 00:27:12 +0900

    style add total using time container

--- src/components/Main/MainContent/TotalTime.tsx
+++ src/components/Main/MainContent/TotalTime.tsx
@@ -7,10 +7,47 @@ type Props = {};
 
 const TotalTime = (props: Props) => {
   return (
-    <Card.Wrapper height="250px" width="510px" borderRadius="8px">
-      <Card.Title>총 사용 시간</Card.Title>
-      <S.Time>140</S.Time>
-      <S.Minute>min</S.Minute>
+    <Card.Wrapper
+      height="250px"
+      width="510px"
+      padding="22px 22px 12px 22px"
+      borderRadius="8px"
+    >
+      <S.TopContainer>
+        <S.TitleContainer>
+          <Card.Title margin="0">총 사용 시간</Card.Title>
+          <S.SubTitle>지난 주보다 20분 ↑</S.SubTitle>
+        </S.TitleContainer>
+        <S.UsageTimeContainer>
+          <S.Time>140</S.Time>
+          <S.TimeLabel>시간</S.TimeLabel>
+          <S.Time>00</S.Time>
+          <S.TimeLabel>분</S.TimeLabel>
+        </S.UsageTimeContainer>
+      </S.TopContainer>
+      <S.RankListContainer>
+        <S.RankList>
+          <S.RankListNameWrapper>
+            <S.Rank>1</S.Rank>
+            <S.Site>site name</S.Site>
+          </S.RankListNameWrapper>
+          <S.UsingTime>000시간 00분</S.UsingTime>
+        </S.RankList>
+        <S.RankList>
+          <S.RankListNameWrapper>
+            <S.Rank>2</S.Rank>
+            <S.Site>site name</S.Site>
+          </S.RankListNameWrapper>
+          <S.UsingTime>000시간 00분</S.UsingTime>
+        </S.RankList>
+        <S.RankList>
+          <S.RankListNameWrapper>
+            <S.Rank>3</S.Rank>
+            <S.Site>site name</S.Site>
+          </S.RankListNameWrapper>
+          <S.UsingTime>000시간 00분</S.UsingTime>
+        </S.RankList>
+      </S.RankListContainer>
     </Card.Wrapper>
   );
 };
