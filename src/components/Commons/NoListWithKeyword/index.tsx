da2572e029aeb3a73cebaf84713775effe4f9c49
Beomyeon <beomyeon.kim@adriel.ai>
2022-08-15 06:09:17 +0900

    apply no result with date range

--- src/components/Commons/NoListWithKeyword/index.tsx
+++ src/components/Commons/NoListWithKeyword/index.tsx
@@ -13,9 +13,15 @@ const NoListWithKeyword = ({ keyword }: Props) => {
     <S.Wrapper>
       <S.ContentWrapper>
         <S.Img src={NoResultImg} />
-        <S.Description>
-          <b>{keyword}</b>의 방문기록이 없어요.
-        </S.Description>
+
+        {keyword ? (
+          <S.Description>
+            <b>{keyword}</b>의 방문기록이 없어요.
+          </S.Description>
+        ) : (
+          <S.Description>적용된 기간에 방문기록이 없어요.</S.Description>
+        )}
+
         <S.Description>검색어를 다시 확인해주시겠어요?</S.Description>
       </S.ContentWrapper>
     </S.Wrapper>
