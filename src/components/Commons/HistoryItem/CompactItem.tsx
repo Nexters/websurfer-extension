f27c7a58f03e97b9f0b17a4131eb8e7c35bbe0f7
DABIN JEONG <41@b41.kr>
2022-08-15 11:46:24 +0900

    fix Error handling in the absence of a favicon url

--- src/components/Commons/HistoryItem/CompactItem.tsx
+++ src/components/Commons/HistoryItem/CompactItem.tsx
@@ -6,16 +6,15 @@ import { LogoIcon } from '@assets/img/svg-icon-paths';
 import { HistoryEntity } from '@redux/webSerfer.type';
 
 const CompactItem = ({ title, href, website }: HistoryEntity) => {
-  const { faviconUrl } = website;
   return (
     <S.CompactItemWrapper onClick={() => window.open(href, '_blank')}>
       {
         <S.ItemIcon
           alt="history-icon"
           src={
-            faviconUrl?.startsWith('/images')
+            website?.faviconUrl?.startsWith('/images')
               ? LogoIcon
-              : faviconUrl || LogoIcon
+              : website?.faviconUrl || LogoIcon
           }
         />
       }
