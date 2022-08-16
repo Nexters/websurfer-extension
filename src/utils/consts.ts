9cd552d93275e9091cc9393335bdcc59ed83142d
Beomyeon <beomyeon.kim@adriel.ai>
2022-08-16 22:52:55 +0900

    add oauth urls for banned url

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,4 +3,8 @@ export const BANNED_URLS_PREFIX = [
   'chrome-extension://',
   'http://localhost/',
   'https://websurfer-web.vercel.app/',
+  'https://accounts.google.com',
+  'https://nid.naver.com/',
+  'https://kauth.kakao.com/',
+  'https://graph.facebook.com/oauth',
 ];
