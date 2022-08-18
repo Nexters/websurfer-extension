189c64434da5a973748ff398a263e197ea1d941d
Beomyeon <beomyeon.kim@adriel.ai>
2022-08-18 23:27:15 +0900

    apply logo

--- src/components/Commons/Logo.tsx
+++ src/components/Commons/Logo.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import * as S from './Logo.styled';
 
+import { FullLogo } from '@assets/img/svg-icon-paths';
+
 type Props = {};
 
 const Logo = (props: Props) => {
-  return <S.Logo>Web surfer</S.Logo>;
+  return <S.Logo src={FullLogo} alt="full-logo"></S.Logo>;
 };
 
 export default Logo;
